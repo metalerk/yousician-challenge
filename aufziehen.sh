d6ae610abe9ca6dca3dea4b6562ad4c373313d23
Luis Esteban <rodriguezjluis0@gmail.com>
2017-09-09 14:19:56 -0700

    add startup script from metalerk/aufziehen

---
+++ aufziehen.sh
@@ -0,0 +1,41 @@
+###	Script just to use in local environments
+
+start_env(){
+	echo
+	echo "    ### Startup script powered by Luis Esteban ###"
+	echo
+	echo "*** Setting development environment..."
+	echo "*** Loading variables..."
+
+	# Reads and loads environmental variables from the .env file
+	export $(cat .env | xargs)
+
+	echo "*** Starting development server..."
+	echo
+}
+
+# From this point no questions, right ? >--(d ¬.¬)--<
+echo
+echo "         Yousician Challenge Startup Script"
+echo
+
+if [ "$1" == "test" ]; then
+	start_env
+	python -m unittest discover
+
+elif [ "$1" == "dev" ]; then
+	start_env
+	# Test dev mode (default port: 5000)
+	python3 app.py
+
+elif [ "$1" == "prod" ]; then
+	start_env
+	# Test prod mode
+	gunicorn -w4 app:app -b 0.0.0.0:8080
+
+else
+	echo "Invalid option."
+	echo
+	echo "usage: ./start_dev.sh test|dev|prod"
+
+fi
